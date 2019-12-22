5e9b73251ef411bf310b9d181e6ecc0adc2bebaa
MarcoPolokk <58775058+MarcoPolokk@users.noreply.github.com>
2019-12-22 19:31:42 +0100

    Initial Commit

---
+++ Cool Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cool Chat
+//
+//  Created by Paweł Kozioł on 22/12/2019.
+//  Copyright © 2019 Paweł Kozioł. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
