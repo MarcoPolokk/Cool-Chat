940002b3d855eeab80b5e1ee194967fa4b622140
MarcoPolokk <58775058+MarcoPolokk@users.noreply.github.com>
2019-12-26 20:31:28 +0100

    AutoLayout and Icons Design
    Add AutoLayout and Icons Design

---
+++ Cool Chat/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Cool Chat
+//
+//  Created by Paweł Kozioł on 25/12/2019.
+//  Copyright © 2019 Paweł Kozioł. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
---
+++ Cool Chat/Views/MessageCell.swift
@@ -0,0 +1,30 @@
+//
+//  MessageCell.swift
+//  Cool Chat
+//
+//  Created by Paweł Kozioł on 26/12/2019.
+//  Copyright © 2019 Paweł Kozioł. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
